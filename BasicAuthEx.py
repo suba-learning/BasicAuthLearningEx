e56272f895377f314fb109c1ee48b892bb2f22be
Suba Narayanan <ksuba.learning@gmail.com>
2025-02-25 21:52:10 -0800

    Added requests with basi auth

--- BasicAuthEx.py
+++ BasicAuthEx.py
@@ -1,7 +1,53 @@
+import json
+
 import requests
+# https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28
 
-url = "https://postman-echo.com/basic-auth"
-response = requests.get(url, auth=("postman", "password"))
+baseurl = "https://api.github.com/user"
+username = "@suba-learning"
+token = "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"
 
-print(f"Status Code: {response.status_code}")
+
+#Get userinfo without the token
+response = requests.get(baseurl)
+print("Status: ",response.status_code)
+print(response.json())
+
+#Get User info
+response = requests.get(baseurl, auth=(username,token))
+print("Status: ",response.status_code)
+print(response.json())
+
+#Get User info with incorrect authkey
+response = requests.get(baseurl, auth=(username,"ghp_SH6d0dWeD4a1PDTytPaJSdqmQCeF9q2W"))
+print("Status: ",response.status_code)
+print(response.json())
+
+#Get Username
+response = requests.get(baseurl, auth=(username,token))
+print("Status: ",response.status_code)
+print("User Name: ",response.json()["login"])
+
+#Get repos of user
+response = requests.get(baseurl+"/repos", auth=(username,token))
+print("Status: ",response.status_code)
+print(response.json())
+
+#Get repos of user
+response =  requests.get("https://api.github.com/users/suba-learning/repos", auth=(username,token))
+print("Status: ",response.status_code)
+print(response.json())
+
+#Create a repo
+payload = {
+    "name":"Hello-World",
+    "description":"This is first repo created via python Api requests!",
+    "private":False
+}
+headers= {
+    "Accept":"application/vnd.github+json"
+}
+response = requests.post(baseurl, auth=(username,token), data=json.dumps(payload))
+print("********")
+print("Status: ",response.status_code)
 print(response.json())
